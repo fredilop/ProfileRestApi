a39c30e6ec3f8e0f50cbbda147d348491dcdbf95
Alfredo Lopez <alfredolopez02@gmail.com>
2022-05-24 00:19:08 -0600

    Adding HTTP API methods and serializers

---
+++ profiles_project/profiles_api/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+class HelloSerializer(serializers.Serializer):
+    """Serializes a name field for testing our HelloApiView"""
+    name = serializers.CharField(max_length=10)
--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,13 +1,19 @@
-from urllib import response
 from django.shortcuts import render
 
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from profiles_api import serializers
 
 # Create your views here.
+
+
 class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializer
+
     """Test API View"""
+
     def get(self, request, format=None):
         """Returns a list of APIView features"""
         an_apiview = [
@@ -21,3 +27,32 @@ class HelloApiView(APIView):
             'message': 'Hello!',
             'an_apiview': an_apiview
         })
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        print("ALFLAG")
+        print(request.data)
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        """ Patch: update only fields that were provided in the request"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Handle delete of an object"""
+        return Response({'method': 'DELETE'})
