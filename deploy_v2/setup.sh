5b41f79d29447e1222d4b3e5637ebf09121ff5d6
Alfredo Lopez <alfredolopez02@gmail.com>
2022-05-27 11:17:33 -0600

    More config playing

--- deploy_v2/setup.sh
+++ deploy_v2/setup.sh
@@ -14,10 +14,10 @@ echo "ALFLAG -> Constant variables... DONE"
 locale-gen en_GB.UTF-8
 
 # Install Python, SQLite and pip
-echo "Installing dependencies..."
+echo "ALFLAG -> Installing dependencies..."
 apt-get update
 apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
-echo "Installing dependencies... DONE"
+echo "ALFLAG -> Installing dependencies... DONE"
 
 echo "ALFLAG -> Clonning project..."
 mkdir -p $PROJECT_BASE_PATH
@@ -30,7 +30,8 @@ python3 -m venv $VIRTUALENV_BASE_PATH/profiles_api
 echo "ALFLAG -> Virtual env... DONE"
 
 echo "ALFLAG -> Installing requirements txt..."
-$VIRTUALENV_BASE_PATH/profiles_api/bin/ sudo pip install -r $PROJECT_BASE_PATH/profiles-rest-api/requirements.txt
+cd $VIRTUALENV_BASE_PATH/profiles_api/bin/ 
+sudo pip install -r $PROJECT_BASE_PATH/profiles-rest-api/requirements.txt
 echo "ALFLAG -> Installing requirements txt... DONE"
 
 # Run migrations
@@ -40,7 +41,7 @@ echo "ALFLAG -> Run migrations... DONE"
 
 echo "ALFLAG -> Supervisor setup..."
 # Setup Supervisor to run our uwsgi process.
-cp $PROJECT_BASE_PATH/profiles-rest-api/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+cp $PROJECT_BASE_PATH/profiles-rest-api/deploy_v2/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
@@ -48,7 +49,7 @@ echo "ALFLAG -> Supervisor setup... DONE"
 
 echo "ALFLAG -> Nginx..."
 # Setup nginx to make our application accessible.
-cp $PROJECT_BASE_PATH/profiles-rest-api/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+cp $PROJECT_BASE_PATH/profiles-rest-api/deploy_v2nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
 systemctl restart nginx.service
